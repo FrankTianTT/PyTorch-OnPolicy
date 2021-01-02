a3a53352c99aac6600d2a86fed86570acc5aa7f1
franktian <franktian424@qq.com>
2021-01-02 17:40:45 +0800

    start a2c, ppo and trpo

---
+++ src/on_policy/a2c.py
@@ -0,0 +1,29 @@
+import torch
+import gym
+import numpy as np
+from on_policy.on_policy_base import OnPolicyBase
+
+
+class A2C(OnPolicyBase):
+    def __int__(self,
+                 env: gym.Env,
+                 critic_network_config=None,
+                 actor_network_config=None,
+                 device="auto",):
+        super(A2C, self).__init__(env, critic_network_config, actor_network_config, device)
+
+    def train(self):
+        pass
+
+
+if __name__ == '__main__':
+    env = gym.make("Humanoid-v3")
+    model = A2C(env)
+
+    obs = env.reset()
+    while True:
+        action = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        env.render()
+        if done:
+            obs = env.reset()
